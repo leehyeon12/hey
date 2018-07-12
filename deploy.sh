b0dd560a2f48e37da3fab0e1c302b28627fc07aa
ruucm <ruucm.a@gmail.com>
2018-07-12 22:01:46 +0700

    deploy url

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ main() {
   default_email=${GIT_DEPLOY_EMAIL:-leehyun6691@gmail.com}
 
   #repository to deploy to. must be readable and writable.
-  repo=${GIT_DEPLOY_REPO:-git@github.com:leehyeon12/Lee9012.git}
+  repo=${GIT_DEPLOY_REPO:-git@github.com:leehyeon12/harbor-study.git}
 
   #append commit hash to the end of message by default
   append_hash=true
